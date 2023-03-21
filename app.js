4550c2640c4ecb35566e04dae5a67f478570b5b4
SirShagalot <56863638+SirShagalot@users.noreply.github.com>
2023-03-21 15:25:33 +0100

    add tables

--- app.js
+++ app.js
@@ -1,22 +1,58 @@
 window.addEventListener("load", initApp);
 
 function initApp() {
+  const charImage =
+    "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.flickr.com%2Fphotos%2F145648817%40N06%2F29130775290&psig=AOvVaw2HaC1U2UZuQ855jTq_lXlE&ust=1679491093531000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCPC544OO7f0CFQAAAAAdAAAAABAD";
+  const charName = "Charizard";
+  const charGeneration = "First";
+  const charElement = "Fire";
+  const charType = "Flying";
+  const charCap = 3;
+  const charLvl = 3;
+  const charHeight = "";
+  const charWeigt = "";
+  const charApperances = "";
+
   createPokemon(
-    image,
-    pokemonName,
-    generation,
-    pokemonElement,
-    pokemonType,
-    evolutonCap,
-    evolutionLvl,
-    pokemonHeigt,
-    pokemonWeight,
-    apperances
+    charImage,
+    charName,
+    charGeneration,
+    charElement,
+    charType,
+    charCap,
+    charLvl,
+    charHeight,
+    charWeigt,
+    charApperances
   );
+
+  // createPokemon(
+  //   image,
+  //   pokemonName,
+  //   generation,
+  //   pokemonElement,
+  //   pokemonType,
+  //   evolutonCap,
+  //   evolutionLvl,
+  //   pokemonHeigt,
+  //   pokemonWeight,
+  //   apperances
+  // );
 }
 
-function createPokemon() {
-  const html = `
+function createPokemon(
+  image,
+  pokemonName,
+  generation,
+  pokemonElement,
+  pokemonType,
+  evolutonCap,
+  evolutionLvl,
+  pokemonHeigt,
+  pokemonWeight,
+  apperances
+) {
+  const html = /*html*/ `
 <tr>
 <td>${image}</td>
 <td>${pokemonName}</td>
@@ -30,6 +66,8 @@ function createPokemon() {
 <td>${apperances}</td>
 </tr>
 `;
+
+  document.querySelector("#pokemonList").insertAdjacentHTML("beforeend", html);
 }
 
 function createList(apperances) {
